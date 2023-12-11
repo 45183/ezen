d5b8c0aed8bf01d82b64978613c3804e7d49d0d6
45183 <hsb9743@naver.com>
2023-12-11 10:36:43 +0900

    day37

--- day37/1208_js_오후_sol/solution-2.js
+++ day37/1208_js_오후_sol/solution-2.js
@@ -9,12 +9,13 @@ const luckyNumber = {
 button.addEventListener("click", () => {
   let { digitCount, maxNumber } = luckyNumber;
   let myNumber = new Set(); // 숫자가 중복되지 않게 저장할 Set 생성
-  for (let i = 0; i < digitCount; i++) {
+  // for (let i = 0; i < digitCount; i++) {  // 중복의 경우 6개보다 적게 출력됨 - 수정 필요
+  //   myNumber.add(Math.floor(Math.random() * maxNumber) + 1);
+  // }
+  while(myNumber.size < digitCount) {  // 중복의 경우 6개보다 적게 출력됨 - 수정 필요
     myNumber.add(Math.floor(Math.random() * maxNumber) + 1);
   }
   result.innerText = `${[...myNumber]}`;  // Set을 Array로 변환
 });
 
 
-// 중복의 경우 6개보다 적게 출력됨
-// 수정 필요
