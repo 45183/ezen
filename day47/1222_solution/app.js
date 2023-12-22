e37d07aedf44270aca640dbc3b0ec8e011d2a826
45183 <hsb9743@naver.com>
2023-12-22 17:40:22 +0900

    day47

---
+++ day47/1222_solution/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const path = require('path');
+
+const defaultRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants');
+
+const app = express();
+// ejs 사용설정
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', defaultRoutes);
+app.use('/', restaurantRoutes);
+
+app.use(function (req, res) {
+  res.status(404).render('404');
+});
+
+app.use(function (error, req, res, next) {
+  res.status(500).render('500');
+});
+
+app.listen(3000);
